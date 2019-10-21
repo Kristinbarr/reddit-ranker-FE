7983b06becd6bd8fd4ec2094bf0d2e26191e17de
Vyraal1 <kevin.afable@gmail.com>
2019-10-21 17:04:03 -0400

    set up skeleton of reducer and actions

--- post-here-frontend/src/actions/index.js
+++ post-here-frontend/src/actions/index.js
@@ -1,7 +1,24 @@
 import axios from "axios";
 
+// FETCHING
+export const FETCHING_START = "FETCHING_START";
+export const FETCHING_SUCCESS = "FETCHING_SUCCESS";
+export const FETCHING_FAIL = "FETCHING_FAIL";
+
+// POSTING
+export const POST_START = "POST_START";
+export const POST_SUCCESS = "POST_SUCCESS";
+export const POST_FAIL = "POST_FAIL";
+
+// DELETING POSTS
+export const DELETE_START = "DELETE_START";
+export const DELETE_SUCCESS = "DELETE_SUCCESS";
+export const DELETE_FAIL = "DELETE_FAIL";
+
 // SIGN-UP
-export const ADD_USER = "ADD_USER";
+export const addUser = user => dispatch => {
+  console.log("adding the following user", user);
+};
 
 // LOG-IN
 
---
+++ post-here-frontend/src/reducers/index.js
@@ -0,0 +1,16 @@
+import {
+  FETCHING_START,
+  FETCHING_SUCCESS,
+  FETCHING_FAIL,
+  POST_START,
+  POST_SUCCESS,
+  POST_FAIL,
+  DELETE_START,
+  DELETE_SUCCESS,
+  DELETE_FAIL
+} from "actions";
+
+const initialState = {
+  drafts: [],
+  error: null
+};
