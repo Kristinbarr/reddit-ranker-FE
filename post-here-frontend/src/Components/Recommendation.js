e96cdde68f9de05ae9c9af3608344f25ae4f9447
Vyraal1 <kevin.afable@gmail.com>
2019-10-21 17:04:18 -0400

    added components for recommendations

---
+++ post-here-frontend/src/Components/Recommendation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// a recommendation consists of its rating, the url, along with any metadata
+
+const Recommendation = props => {};
+
+export default Recommendation;
---
+++ post-here-frontend/src/Components/SavedPosts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//needs to have the users saved posts
+//each post will have the title and the body
+// * do I want to include when the post was saved?
+
+const SavedPosts = () => {
+  return <div>Saved Posts</div>;
+};
+
+export default SavedPosts;
