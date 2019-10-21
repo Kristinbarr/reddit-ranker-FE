ba850c0c004c5dee44bfa0b15b0e897ad7b6abe5
Jackson McComas <jackson.mccomas@gmail.com>
2019-10-21 13:34:48 -0500

    Added Home component

---
+++ post-here-frontend/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from "react-router";
+
+
+const Home = () => {
+    
+    return (
+        <div>
+            <h1>Welcome to the Reddit post finder app!</h1>
+            <button>Login Here!</button>
+            <button>Sign Up Here!</button>
+        </div>
+    )
+}
+
+export default Home;
