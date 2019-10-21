e32540fdbcda70e917f00a96a47591f0ce2cdbf0
Vyraal1 <kevin.afable@gmail.com>
2019-10-21 13:55:03 -0400

    quick commit before debugging

--- post-here-frontend/src/actions/index.js
+++ post-here-frontend/src/actions/index.js
@@ -1,6 +1,11 @@
+import axios from "axios";
+
+// SIGN-UP
 export const ADD_USER = "ADD_USER";
 
-// POSTS
+// LOG-IN
+
+// DRAFTS
 export const ADD_POST = "ADD_POST";
 export const DELETE_POST = "DELETE_POST";
 export const EDIT_POST = "EDIT_POST";
