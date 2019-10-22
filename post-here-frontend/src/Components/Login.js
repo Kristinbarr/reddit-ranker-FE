7266804bc7dc29d57f89238717a9352bf02ae122
Vyraal1 <kevin.afable@gmail.com>
2019-10-21 23:25:13 -0400

    correcting an import

--- post-here-frontend/src/Components/Login.js
+++ post-here-frontend/src/Components/Login.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { withFormik, Form, Field} from 'formik'
+import React from "react";
+import { withFormik, Form, Field } from "formik";
 // import * as Yup from 'yup';
-// import axios from 'axios';
 
 const Login = () => {
-   
-    return (
-     
-        <Form>
-            <label>Email:  <Field type="email" placeholder="Email" name="email"></Field></label> <br/>
-            <label>Password:  <Field type="password" placeholder="Password" name="password"></Field></label> <br/>
-            <button type="submit">Login</button>
-        </Form>
-    
-    )
-}
+  return (
+    <Form>
+      <label>
+        Email: <Field type="email" placeholder="Email" name="email"></Field>
+      </label>{" "}
+      <br />
+      <label>
+        Password:{" "}
+        <Field type="password" placeholder="Password" name="password"></Field>
+      </label>{" "}
+      <br />
+      <button type="submit">Login</button>
+    </Form>
+  );
+};
 
 const FormikApp = withFormik({
-    mapPropsToValues({email, password}) {
-        return{
-            email: email || '',
-            password: password || ''
-        }
-    },
-    handleSubmit(values) {
-        console.log(values)
-    }
-})(Login)
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(Login);
 
 export default FormikApp;
--- post-here-frontend/src/Components/Signup.js
+++ post-here-frontend/src/Components/Signup.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import { withFormik, Form, Field} from 'formik'
-// import * as Yup from 'yup';
-// import axios from 'axios';
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+
+// connecting to store and actions
+import { registerUser } from "../actions";
+import { connect } from "react-redux";
 
 const SignUp = () => {
-   
-    return (
-     
-        <Form>
-            <label>Email:  <Field type="email" placeholder="Email" name="email"></Field></label> <br/>
-            <label>Password:  <Field type="password" placeholder="Password" name="password"></Field></label> <br/>
-            <button type="submit">Sign Up</button>
-        </Form>
-    
-    )
-}
+  return (
+    <Form>
+      <label>
+        Email: <Field type="email" placeholder="Email" name="email"></Field>
+      </label>{" "}
+      <br />
+      <label>
+        Password:{" "}
+        <Field type="password" placeholder="Password" name="password"></Field>
+      </label>{" "}
+      <br />
+      <button type="submit">Sign Up</button>
+    </Form>
+  );
+};
 
 const FormikApp = withFormik({
-    mapPropsToValues({email, password}) {
-        return{
-            email: email || '',
-            password: password || ''
-        }
-    },
-    handleSubmit(values) {
-        console.log(values)
-    }
-})(SignUp)
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(SignUp);
 
-export default FormikApp;
+const mapStateToProps = state => {
+  return {
+    error: state.error
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(FormikApp);
