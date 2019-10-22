b364946837e8c3ed10cc184bae5b61e7fcff3d9e
Vyraal1 <kevin.afable@gmail.com>
2019-10-21 23:11:28 -0400

    util routes for authentication

--- post-here-frontend/src/utils/axiosWithAuth.js
+++ post-here-frontend/src/utils/axiosWithAuth.js
@@ -1,3 +1,14 @@
 import axios from "axios";
 
-export const axiosWithAuth = () => {};
+const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    baseURL: "https://reddit-ranker.herokuapp.com/api/auth",
+    headers: {
+      Authorization: token
+    }
+  });
+};
+
+export default axiosWithAuth;
