181e527273ff9a19b7377a70c04085ff24b46c88
Vyraal1 <kevin.afable@gmail.com>
2019-10-22 18:47:14 -0400

    starting up saved post component

--- post-here-frontend/src/Components/SavedPosts.js
+++ post-here-frontend/src/Components/SavedPosts.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { getSavedPosts } from "../actions";
 
 //needs to have the users saved posts
 //each post will have the title and the body
 // * do I want to include when the post was saved?
 
-const SavedPosts = () => {
-  return <div>Saved Posts</div>;
+const SavedPosts = props => {
+  const initialState = [];
+  const [savedPosts, setSavedPosts] = useState(initialState);
+  const id = localStorage.getItem("id");
+
+  useEffect(() => {
+    setSavedPosts(getSavedPosts());
+  }, []);
+
+  return <div classdN>Saved Posts</div>;
 };
 
-export default SavedPosts;
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { getSavedPosts }
+)(SavedPosts);
