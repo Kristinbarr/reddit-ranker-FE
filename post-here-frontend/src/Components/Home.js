743d190b8e6e32e11ba39ff4f8d41be2a894cc90
Vyraal1 <kevin.afable@gmail.com>
2019-10-22 00:39:34 -0400

    linter formatting, sorry Jackson, will get prettierrc set up

--- post-here-frontend/src/Components/Home.js
+++ post-here-frontend/src/Components/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Home = () => {
-    
-    return (
-        <div>
-            <h1>Welcome to the Reddit post finder app!</h1>
-            <Link to="./Login"><button>Login Here!</button></Link>
-            <Link to="./Signup"><button>Sign Up Here!</button></Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Welcome to the Reddit post finder app!</h1>
+      <Link to="./Login">
+        <button>Login Here!</button>
+      </Link>
+      <Link to="./Signup">
+        <button>Sign Up Here!</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Home;
