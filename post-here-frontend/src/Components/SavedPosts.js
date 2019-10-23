1ad727f5079c3e8cc9d54578998e9fba8d57d844
Vyraal1 <kevin.afable@gmail.com>
2019-10-23 10:38:30 -0400

    gets the savedPosts from the backend

--- post-here-frontend/src/Components/SavedPosts.js
+++ post-here-frontend/src/Components/SavedPosts.js
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
+
 import { getSavedPosts } from "../actions";
+import { connect } from "react-redux";
 
 //needs to have the users saved posts
 //each post will have the title and the body
 // * do I want to include when the post was saved?
 
 const SavedPosts = props => {
+  console.log("props of the SavedPosts", props);
   const initialState = [];
   const [savedPosts, setSavedPosts] = useState(initialState);
   const id = localStorage.getItem("id");
 
   useEffect(() => {
-    setSavedPosts(getSavedPosts());
-  }, []);
+    setSavedPosts(getSavedPosts(id));
+  }, [id]);
 
-  return <div classdN>Saved Posts</div>;
+  if (!savedPosts) {
+    return <div className>You have no saved posts</div>;
+  } else {
+    return (
+      <div>
+        {savedPosts.map(savedPost => {
+          console.log(savedPost);
+        })}
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = state => {
-  return {};
+  return { initialState: state.drafts };
 };
 
 export default connect(
