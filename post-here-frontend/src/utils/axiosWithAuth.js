3d22aa3225e7dad86ec9b43676e0b124590133f4
Vyraal1 <kevin.afable@gmail.com>
2019-10-23 10:39:00 -0400

    altered base URL to not need auth

--- post-here-frontend/src/utils/axiosWithAuth.js
+++ post-here-frontend/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "https://reddit-ranker.herokuapp.com/api/auth",
+    baseURL: "https://reddit-ranker.herokuapp.com/api/",
     headers: {
       Authorization: token
     }
