5d75894f8bc2365e36236dfe407b2068f77392d2
Jackson McComas <jackson.mccomas@gmail.com>
2019-10-23 23:13:47 -0500

    messed with the reccomended

--- post-here-frontend/src/Components/RecommendationList.js
+++ post-here-frontend/src/Components/RecommendationList.js
@@ -1,110 +1,102 @@
-import React, { useState, useEffect } from "react";
-import Recommendation from "./Recommendation";
-import { connect } from "react-redux";
-import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Title from "./Title";
-import { getRecommendations } from "../actions";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import Recommendation from './Recommendation';
+import { connect } from 'react-redux';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Title from './Title';
+import { getRecommendations } from '../actions';
+import styled from 'styled-components';
 
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
 
 const TableWrapper = styled.div`
-  width: 30%;
-  margin: 0 auto;
+	width: 30%;
+	margin: 0 auto;
 `;
 
 const TableContentWrapper = styled.div`
-  border-spacing: 10px;
+	border-spacing: 10px;
+	overflow: hidden;
 `;
 
 // const TableCellWrapper = styled.div`
 
 // `
 const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    overflowX: "auto"
-  },
-  table: {
-    minWidth: 650
-  }
+	root: {
+		width: '95%',
+		overflowX: 'auto'
+	},
+	table: {
+		minWidth: 650
+	}
 });
 
 // Generate Order Data
 function createData(score, subreddit) {
-  return { score, subreddit };
+	return { score, subreddit };
 }
 
 const data = [
-  createData(1, "r/CasualConversation"),
-  createData(2, "r/dogpark"),
-  createData(3, "r/DogTraining"),
-  createData(4, "r/Dogs"),
-  createData(5, "r/friendship")
+	createData(1, 'r/CasualConversation'),
+	createData(2, 'r/dogpark'),
+	createData(3, 'r/DogTraining'),
+	createData(4, 'r/Dogs'),
+	createData(5, 'r/friendship')
 ];
 
-const RecommendationList = props => {
-  console.log("recommendationlist props", props);
-  const { getRecommendations, loggedInUser } = props;
-  const initialState = data;
-  const [recs, setRecs] = useState(initialState);
+const RecommendationList = (props) => {
+	console.log('recommendationlist props', props);
+	const { getRecommendations, loggedInUser } = props;
+	const initialState = data;
+	const [ recs, setRecs ] = useState(initialState);
 
-  // useEffect(() => {
-  //   setRecs(getRecommendations(loggedInUser));
-  // }, [loggedInUser]);
+	// useEffect(() => {
+	//   setRecs(getRecommendations(loggedInUser));
+	// }, [loggedInUser]);
 
-  const [recommendations, setRecommendations] = useState([]);
-  // if recommendations is empty, then render "GENERATE RECOMMENDATIONS ON THE LEFT"
-  const classes = useStyles();
-  console.log("array of posts", recs);
-  return (
-    <TableWrapper>
-      <Title>Top Suggested Subreddits</Title>
-      <Paper className={classes.root}>
-        <TableContentWrapper>
-          <Table
-            className={classes.table}
-            aria-label="simple table"
-            size="small"
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell style={{ height: "60px" }}>Rank</TableCell>
-                <TableCell style={{ height: "60px" }}>Subreddit</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {recs.map(rec => (
-                <TableRow key={rec.subreddit}>
-                  <TableCell style={{ height: "60px" }}>{rec.score}</TableCell>
-                  <TableCell style={{ height: "60px" }}>
-                    {rec.subreddit}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContentWrapper>
-      </Paper>
-    </TableWrapper>
-  );
+	const [ recommendations, setRecommendations ] = useState([]);
+	// if recommendations is empty, then render "GENERATE RECOMMENDATIONS ON THE LEFT"
+	const classes = useStyles();
+	console.log('array of posts', recs);
+	return (
+		<TableWrapper>
+			<Title>Top Suggested Subreddits</Title>
+			<Paper className={classes.root}>
+				<TableContentWrapper>
+					<Table className={classes.table} aria-label="simple table" size="small">
+						<TableHead>
+							<TableRow>
+								<TableCell style={{ height: '60px' }}>Rank</TableCell>
+								<TableCell style={{ height: '60px' }}>Subreddit</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{recs.map((rec) => (
+								<TableRow key={rec.subreddit}>
+									<TableCell style={{ height: '60px' }}>{rec.score}</TableCell>
+									<TableCell style={{ height: '60px' }}>{rec.subreddit}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContentWrapper>
+			</Paper>
+		</TableWrapper>
+	);
 };
 
-const mapStateToProps = state => {
-  console.log("state passed to recommendation list", state);
-  return {
-    id: state.loggedInUser
-  };
+const mapStateToProps = (state) => {
+	console.log('state passed to recommendation list', state);
+	return {
+		id: state.loggedInUser
+	};
 };
 
-export default connect(
-  mapStateToProps,
-  { getRecommendations }
-)(RecommendationList);
+export default connect(mapStateToProps, { getRecommendations })(RecommendationList);
