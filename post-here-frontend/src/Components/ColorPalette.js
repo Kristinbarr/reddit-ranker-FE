263c2c8b84e33abe22eae47dc742f67f28bf2dbb
Vyraal1 <kevin.afable@gmail.com>
2019-10-23 20:27:09 -0400

    not sure what i changed here, gl jackson

--- post-here-frontend/src/Components/ColorPalette.js
+++ post-here-frontend/src/Components/ColorPalette.js
@@ -1,10 +1,10 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const palette = {
-  primary: { main: '#001e4c' },
-  secondary: { main: '#e86868' }
+  primary: { main: "#001e4c" },
+  secondary: { main: "#e86868" }
 };
-const themeName = 'Prussian Blue Sunglo Hyrax';
-console.log(createMuiTheme({ palette, themeName }))
+const themeName = "Prussian Blue Sunglo Hyrax";
+console.log(createMuiTheme({ palette, themeName }));
 
 export default createMuiTheme({ palette, themeName });
