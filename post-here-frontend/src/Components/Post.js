42850a73822e2dad1de9701c359f9c61abe9a3e4
Jackson McComas <jackson.mccomas@gmail.com>
2019-10-23 22:27:19 -0500

    edited some text to better amtch component

--- post-here-frontend/src/Components/Post.js
+++ post-here-frontend/src/Components/Post.js
@@ -1,130 +1,116 @@
-import React from "react";
-import { withFormik, Form } from "formik";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import { withFormik, Form } from 'formik';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 
 const PostTextWrapper = styled.div`
-    width: 60%;
-    margin: 0 auto;
-`
+	width: 60%;
+	margin: 0 auto;
+`;
 
 const ButtonsWrapper = styled.div`
-    margin-top: 10px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-`
-const useStylesReddit = makeStyles(theme => ({
-  root: {
-    border: "1px solid #e2e2e1",
-    overflow: "hidden",
-    borderRadius: 4,
-    backgroundColor: "#fcfcfb",
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
-    "&:hover": {
-      backgroundColor: "#fff"
-    },
-    "&$focused": {
-      backgroundColor: "#fff",
-      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-      borderColor: theme.palette.primary.main
-    }
-  },
-  focused: {}
+	margin-top: 10px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+`;
+const useStylesReddit = makeStyles((theme) => ({
+	root: {
+		border: '1px solid #e2e2e1',
+		overflow: 'hidden',
+		borderRadius: 4,
+		backgroundColor: '#fcfcfb',
+		transition: theme.transitions.create([ 'border-color', 'box-shadow' ]),
+		'&:hover': {
+			backgroundColor: '#fff'
+		},
+		'&$focused': {
+			backgroundColor: '#fff',
+			boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+			borderColor: theme.palette.primary.main
+		}
+	},
+	focused: {}
 }));
 
 function RedditTextField(props) {
-  const classes = useStylesReddit();
+	const classes = useStylesReddit();
 
-  return (
-    <TextField InputProps={{ classes, disableUnderline: true }} {...props} />
-  );
+	return <TextField InputProps={{ classes, disableUnderline: true }} {...props} />;
 }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  margin: {
-    margin: theme.spacing(1)
-  }
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap'
+	},
+	margin: {
+		margin: theme.spacing(1)
+	}
 }));
 
 const Post = ({ values, handleChange }) => {
-  const classes = useStyles();
-  return (
-    <PostTextWrapper>
-        <h1>Welcome to the Reddit post finder app!</h1>
-      <Form>
-            <RedditTextField
-              label="Reddit Title Here"
-              name="title"
-              className={classes.margin}
-              variant="filled"
-              id="reddit-input"
-              fullWidth
-              onChange={handleChange}
-              value={values.title}
-            />{" "}
-            <br />
-            <RedditTextField
-              label="Reddit Post Here"
-              name="post"
-              className={classes.margin}
-              variant="filled"
-              multiline
-              rows="16"
-              fullWidth
-              id="reddit-input"
-              onChange={handleChange}
-              value={values.post}
-            />
-        
-        <ButtonsWrapper>
-            <Button
-              variant="outlined"
-              className={classes.button}
-              onClick={()=>{console.log("Deleting from server...")}}
-            >
-              Delete
-            </Button>
-            
-            <Button
-              variant="outlined"
-              className={classes.button}
-              color="primary"
-              type="submit"
-            >
-              Submit For Recommendation
-            </Button>
-    
-            <Button
-              variant="outlined"
-              className={classes.button}
-              color="secondary"
-              onClick={()=>{console.log("Saving to server...")}}
-            >
-              Save
-            </Button>
-        </ButtonsWrapper>
-      </Form>
-    </PostTextWrapper>
-  );
+	const classes = useStyles();
+	return (
+		<PostTextWrapper>
+			<h1>Welcome to the Reddit post finder app!</h1>
+			<Form>
+				<RedditTextField
+					label="Reddit Title Here"
+					name="title"
+					className={classes.margin}
+					variant="filled"
+					id="reddit-input"
+					fullWidth
+					onChange={handleChange}
+					value={values.title}
+				/>{' '}
+				<br />
+				<RedditTextField
+					label="Reddit Post Here"
+					name="post"
+					className={classes.margin}
+					variant="filled"
+					multiline
+					rows="16"
+					fullWidth
+					id="reddit-input"
+					onChange={handleChange}
+					value={values.post}
+				/>
+				<ButtonsWrapper>
+					<Button variant="outlined" className={classes.button} color="primary" type="submit">
+						Submit For Recommendation
+					</Button>
+
+					<Button
+						variant="outlined"
+						className={classes.button}
+						color="secondary"
+						onClick={() => {
+							console.log('Saving to server...');
+						}}
+					>
+						Save
+					</Button>
+				</ButtonsWrapper>
+			</Form>
+		</PostTextWrapper>
+	);
 };
 
 const FormikAppPost = withFormik({
-  mapPropsToValues({ title, post }) {
-    return {
-      title: title || "",
-      post: post || ""
-    };
-  },
-  handleSubmit(text) {
-    console.log(text);
-  }
+	mapPropsToValues({ title, post }) {
+		return {
+			title: title || '',
+			post: post || ''
+		};
+	},
+	handleSubmit(text) {
+		console.log(text);
+	}
 })(Post);
 
 export default FormikAppPost;
